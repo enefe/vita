dacaf5e2827070cdf7996ba4ed4089288b0150a0
enefe815 <enefe815@yandex.ru>
2022-06-15 17:47:05 +0300

    feat: feedback

--- js/index.js
+++ js/index.js
@@ -10,3 +10,35 @@ $('.header__open-menu').click(function() {
         $('.header__open-menu').show();
     })
 });
+
+if (window.innerWidth >= 768) {
+    $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            items: 2,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            autoplaySpeed: 2000,
+            pagination: false,
+            dots: true,
+            nav: false,
+        });
+    });
+}
+
+if (window.innerWidth < 768) {
+    $(document).ready(function() {
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            items: 1,
+            autoplay: true,
+            autoplayTimeout: 8000,
+            autoplayHoverPause: true,
+            autoplaySpeed: 4000,
+            pagination: false,
+            dots: true,
+            nav: false,
+        });
+    });
+}
